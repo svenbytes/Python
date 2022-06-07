221c918f1337ee0e38a57fbc68797cdae64a3389
Svenbytes <94544175+svenbytes@users.noreply.github.com>
2022-06-07 08:36:07 +0200

    Add files via upload

---
+++ PasswordGenerator/PasswordGenerator.py
@@ -0,0 +1,43 @@
+#Required to install "secrets", "string", "clipboard" in terminal whit "pip install <Modulname>"
+#Code by Svenbytes
+#github.com/svenbytes/Python
+
+import secrets
+import string
+import clipboard
+chars = string.digits + string.ascii_letters + string.punctuation
+
+
+
+print()
+print()
+print()
+print("     Passwort Generator")
+print("         Passwort Generator")
+print("             ---------------")
+print("        Code by <Svenbytes>")
+print("     Code by <Svenbytes>")
+print()
+print()
+print()
+
+
+while True:
+
+    number = input("How many chars should the password have?: ")		#Request how many chars the password should have.
+    password = ''.join(secrets.choice(chars) for _ in range(int(number)))	#Generates the password string
+
+    print()
+    print()
+    print()
+    print(">--------------------------------------<")
+    print()
+    print("Your password (also copied to clipboard): ")
+    print()
+    print(password)
+    print()
+    print(">--------------------------------------<")
+    print()
+    print()
+    print()
+    clipboard.copy(password)
